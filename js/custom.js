f9fb91d6ceb0a32ceafae26ff356d884c2acda10
rps <r7shekhawat@gmail.com>
2016-12-23 18:28:33 -0600

    adding smooth scrolling to nav bar

--- js/custom.js
+++ js/custom.js
@@ -56,4 +56,24 @@ $(document).ready(function(){
     
 });
 
-
+//add smooth scrolling
+$(document).ready(function() {
+    
+    'use strict';
+   
+    
+    $('.nav-item, #scroll-to-top').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+    
+    
+});
