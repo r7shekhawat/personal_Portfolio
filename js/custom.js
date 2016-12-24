d84ba0359bbff2e6c3c25d7c5162a36d1fba4f51
rps <r7shekhawat@gmail.com>
2016-12-23 18:27:41 -0600

    adding javascript for navbar -hide/show the navbar

---
+++ js/custom.js
@@ -0,0 +1,59 @@
+/* Show and hide menu */
+
+$(document).ready(function(){
+   
+    'use strict';
+    
+    $(window).scroll(function() {
+        
+        'use strict';
+        
+        if($(window).scrollTop() < 80 ) {
+            
+            $('.navbar').css ({
+               'margin-top': '-100px',
+                'opacity': '0'
+                
+            });
+            
+            $('.navbar-default').css({
+                'background-color': 'rgba(59, 59 , 59, 0)'
+                
+            });
+            
+        } else {
+            
+            $('.navbar').css ({
+               'margin-top': '0px',
+                'opacity': '1'
+                
+            });
+            
+            $('.navbar-default').css({
+                'background-color': 'rgba(59, 59 , 59, 0.7)',
+                'border-color': '#444'
+                
+            });
+            
+            $('.navbar-brand img').css({
+               'height': '35px',
+                'padding-top': '0px'
+                
+            });
+            
+            $('.navbar-nav > li > a ').css({
+                'padding-top': '15px'
+                
+                
+            });
+            
+            
+        }
+       
+        
+    });
+    
+    
+});
+
+
